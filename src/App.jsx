c091c3121bb2b9e452e11c99a1fb621e99b888cf
Nick M <mully7773@gmail.com>
2023-02-12 13:47:28 -0600

    Add Globalstyles and some folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/Global/GlobalStyles';
+
+const StyledImg = styled.img`
+  height: 50rem;
+  width: 50rem;
+  transform: scaleX(-1);
+`;
+
 function App() {
-  return <>Hello, Nick!</>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledImg src="public/mount-fuji-hokusai-near-ejiri.svg" />
+    </>
+  );
 }
 
 export default App;
---
+++ src/styles/Global/GlobalStyles.jsx
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    *, *::after, *::before {
+        margin: 0;
+        padding: 0;
+        box-sizing: inherit;
+    }
+
+    html {
+        box-sizing: border-box;
+        font-size: 62.5%;
+        /* color-scheme: dark light; */
+
+        @media only screen and (max-width: 75em) {
+        font-size: 56.25%;
+        }
+
+        @media only screen and (max-width: 56.25em) {
+        font-size: 50%;
+        }
+}
+
+
+
+    body {
+        min-height: 100vh;
+        line-height: 1.6;
+        font-weight: 300;        
+    }
+
+    img, picture, svg, video {
+        display: block;
+        max-width: 100%;
+    }
+
+    button {
+        outline: transparent;
+    }
+`;
+
+export default GlobalStyles;
