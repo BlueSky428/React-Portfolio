96063f02705fb4bc0c8ff6fb30382d070b999729
Nick M <mully7773@gmail.com>
2023-03-29 18:42:39 -0500

    Add information to StyledFooterContent - temp - testing out styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -32,7 +32,32 @@ const StyledFooterBackground = styled.div`
 const StyledFooterContent = styled.div`
   background-color: ${props => props.theme.primary.footerContentBg};
   height: 100%;
-  position: relative;
+  display: grid;
+  grid-template-columns: 35% 65%;
+  /* column-gap: 10rem; */
+  /* grid-template-columns: 1fr 1fr; */
+  align-items: center;
+`;
+
+const StyledNav = styled.ul`
+  justify-self: end;
+  & li {
+    color: #eee;
+    font-family: ${props => props.theme.fonts.secondary};
+    font-size: 2.4rem;
+    text-transform: capitalize;
+  }
+`;
+
+const StyledContact = styled.ul`
+  margin-left: 10rem;
+  margin-top: 5rem;
+  & li {
+    color: #eee;
+    font-family: ${props => props.theme.fonts.secondary};
+    font-size: 2.6rem;
+    /* text-transform: capitalize; */
+  }
 `;
 
 const Footer = () => {
@@ -43,7 +68,18 @@ const Footer = () => {
           <FooterSVG />
         </div>
       </StyledFooterBackground>
-      <StyledFooterContent></StyledFooterContent>
+      <StyledFooterContent>
+        <StyledNav>
+          <li>home</li>
+          <li>about</li>
+          <li>technology</li>
+          <li>projects</li>
+        </StyledNav>
+        <StyledContact>
+          <li>e-mail: test@test.com</li>
+          <li>phone: 503-234-2343</li>
+        </StyledContact>
+      </StyledFooterContent>
     </StyledFooter>
   );
 };
