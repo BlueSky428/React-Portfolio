bbe8b45ef0811d46a9ced961bac895161fc7fc1f
Nick M <mully7773@gmail.com>
2023-04-15 11:05:23 -0500

    Add a loading spinner to Suspense component

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,12 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './i18n';
-import styled from 'styled-components';
+import { StyledLoadingContainer } from './styles/UI/StyledLoadingContainer';
+import SuspenseFallback from './styles/UI/SuspenseFallback';
 
-const StyledLoadingContainer = styled.div`
-  text-align: center;
-  padding: 4rem 0;
-  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-    Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  color: #1e3e67;
-`;
 const loadingMarkup = (
   <StyledLoadingContainer>
-    <h2>Loading...</h2>
+    <SuspenseFallback />
   </StyledLoadingContainer>
 );
 
